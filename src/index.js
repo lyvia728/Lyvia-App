484d1c3e4367248b49a3a387b31d52f15c1f3f5e
Welile Nkomo <welilelyvia@gmail.com>
2025-08-31 23:25:52 +0200

    Added the search functionality using APIs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+} 
+
+
+function searchCity(city) { 
+    let apiKey = "eba2o4c7f9e18e3f1t9e2154f4dd3105";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
